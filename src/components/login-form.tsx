02c01eb4a3ff83b4f214c3c20938fa5530b308f0
Sam Fuchs <samcfuchs@gmail.com>
2026-01-29 10:43:17 -0800

    Another redirect

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -33,7 +33,7 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
       })
       if (error) throw error
       // Update this route to redirect to an authenticated route. The user already has an active session.
-      location.href = '/protected'
+      location.href = '/app'
     } catch (error: unknown) {
       setError(error instanceof Error ? error.message : 'An error occurred')
     } finally {
